64bc84bc3fbde367deeefd47d97be54ebe663927
Mukund Jee Tiwari <92503293+tiwariji-mukund@users.noreply.github.com>
2023-09-08 08:15:24 +0530

    Move All Negative at front.java

---
+++ Move All Negative at front.java
@@ -0,0 +1,22 @@
+<a href "https://www.codingninjas.com/studio/problems/move-all-negative-numbers-to-beginning-and-positive-to-end_1112620">Question Link</a>
+
+public class Solution {
+
+    public static int[] separateNegativeAndPositive(int a[]) {
+        int n=a.length;
+        if(n<1) return a;
+        int i=0, j=n-1;
+        while(i<j){
+            if(a[i]<0) i++;
+            else if(a[j]<0){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+                i++;
+                j--;
+            }
+            else j--;
+        }
+        return a;
+    }
+}
